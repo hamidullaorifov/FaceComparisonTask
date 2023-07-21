6da2dd08c3d701b47679ac55971a4e7c080b2190
name <email>
2023-07-21 16:02:26 +0500

    Fixed spelling mistakes

--- face_comparison_app/views.py
+++ face_comparison_app/views.py
@@ -9,6 +9,6 @@ def index(request):
 
         result = are_same_person(image1, image2)
 
-        return render(request, 'face_comparision_app/index.html', {'result': result})
+        return render(request, 'face_comparison_app/index.html', {'result': result})
     
-    return render(request, 'face_comparision_app/index.html')
+    return render(request, 'face_comparison_app/index.html')
